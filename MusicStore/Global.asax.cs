90f7f2d01fe44fc553bf2675493a2c119a9fd08d
Vi Sokal <sokalmet@ukr.net>
2016-07-01 18:00:23 +0300

    making tutorial p60 done

--- MusicStore/Global.asax.cs
+++ MusicStore/Global.asax.cs
@@ -11,6 +11,7 @@ namespace MusicStore
     {
         protected void Application_Start()
         {
+            System.Data.Entity.Database.SetInitializer(new Models.SampleData());
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
